ae3994c9b875604304b23b1aea5e0ac1038df83c
Di Wu <31394433+wd370175609@users.noreply.github.com>
2017-12-11 10:15:48 -0800

    Create Longest consecutive sequence in Binary tree.java

---
+++ BinaryTree_-_DivideConquer/Longest consecutive sequence in Binary tree.java
@@ -0,0 +1,36 @@
+
+//use traverse to find max value, use divide conquer to store current value;
+class GfG
+{
+    int maxlc=1;
+	public int longestConsecutive(TreeNode root)
+        {
+         //add code here.
+         
+           helper(root);
+           if(maxlc==1)
+            return -1;
+         
+           return maxlc;
+ 
+        }
+    public int helper(TreeNode root){
+         if(root==null){
+                return 0;
+            }
+           
+            int leftlc=helper(root.left);
+            int rightlc=helper(root.right);
+            int subtreelongest=1;
+            if(root.left!=null && root.left.key==root.key+1){
+                subtreelongest=Math.max(subtreelongest,leftlc+1);
+            }
+            else if(root.right!=null && root.right.key==root.key+1)
+                subtreelongest=Math.max(subtreelongest,rightlc+1);
+                
+            if(maxlc<subtreelongest)
+                maxlc=subtreelongest;
+            
+            return subtreelongest;
+    }
+}
