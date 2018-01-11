316b9513e380a290e858cdbf59eef9e2085f0e7b
Di Wu <31394433+wd370175609@users.noreply.github.com>
2018-01-10 17:38:21 -0800

    Update Graph Valid Tree.java

--- BFS/Graph Valid Tree.java
+++ BFS/Graph Valid Tree.java
@@ -1,3 +1,8 @@
+/*Given n nodes labeled from 0 to n - 1 and a list of 
+undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.*/
+
+
+
 public class Solution {
     /*
      * @param n: An integer
