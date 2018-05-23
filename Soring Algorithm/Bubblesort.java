21935e4aeb6f52446996f865d6cbe99e06aba3ce
Di Wu <31394433+wd370175609@users.noreply.github.com>
2018-05-23 11:13:04 -0700

    Create Bubblesort.java

---
+++ Soring Algorithm/Bubblesort.java
@@ -0,0 +1,30 @@
+
+   Best complexity is O(n)
+   Worse is O(n^2)
+   
+   // An optimized version of Bubble Sort
+    static void bubbleSort(int arr[], int n)
+    {
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++) 
+        {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) 
+            {
+                if (arr[j] > arr[j + 1]) 
+                {
+                    // swap arr[j] and arr[j+1]
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+ 
+            // IF no two elements were 
+            // swapped by inner loop, then break
+            if (swapped == false)
+                break;
+        }
+    }
