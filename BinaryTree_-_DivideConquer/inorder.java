8db49e367fe64e001c8dbb05e72a2b308f1352dd
Di Wu <31394433+wd370175609@users.noreply.github.com>
2018-12-13 21:08:54 -0800

    Update inorder.java

--- BinaryTree_-_DivideConquer/inorder.java
+++ BinaryTree_-_DivideConquer/inorder.java
@@ -5,7 +5,7 @@ public List<Integer> inorderTraversal(TreeNode root) {
             return res;
         Stack<TreeNode> stack=new Stack<>();
         TreeNode cur=root;
-        while(cur!=null || !stack.isEmpty()){
+        while(cur!=null || !stack.isEmpty()){           //be careful of the cur!=null here
             while(cur!=null){                   //make sure the most left will be pop first
                 stack.push(cur);
                 cur=cur.left;
